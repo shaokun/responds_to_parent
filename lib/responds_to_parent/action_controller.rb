791257c16047e742ce833519052ae2d23bdabd65
Shaokun Wu <shaokun.wu@gmail.com>
2011-04-19 16:57:07 +0800

    hack to support rails 3, because no erase_redirect_results in rails 3

--- lib/responds_to_parent/action_controller.rb
+++ lib/responds_to_parent/action_controller.rb
@@ -6,22 +6,32 @@ module RespondsToParent
     # to use IFRAME base RPC.
     def responds_to_parent(&block)
       yield
-    
+
       if performed?
-        # Either pull out a redirect or the request body
-        script =  if location = erase_redirect_results
-                    "document.location.href = '#{self.class.helpers.escape_javascript location.to_s}'"
-                  else
-                    response.body || ''
-                  end
-
-        # Clear out the previous render to prevent double render
-        erase_results
-
         # We're returning HTML instead of JS or XML now
         response.headers['Content-Type'] = 'text/html; charset=UTF-8'
 
-        # Eval in parent scope and replace document location of this frame 
+        # Either pull out a redirect or the request body
+        script =  if response.headers['Location']
+                    #TODO: erase_redirect_results is missing in rails 3.0 has to be implemented
+                    #erase redirect
+                    "document.location.href = #{location.to_s.inspect}"
+                  else
+                    response.body
+                  end
+
+        # Escape quotes, linebreaks and slashes, maintaining previously escaped slashes
+        # Suggestions for improvement?
+        script = (script || '').
+          gsub('\\', '\\\\\\').
+          gsub(/\r\n|\r|\n/, '\\n').
+          gsub(/['"]/, '\\\\\&').
+          gsub('</script>','</scr"+"ipt>')
+
+        # Clear out the previous render to prevent double render
+        response.request.env['action_controller.instance'].instance_variable_set(:@_response_body, nil)
+
+        # Eval in parent scope and replace document location of this frame
         # so back button doesn't replay action on targeted forms
         # loc = document.location to be set after parent is updated for IE
         # with(window.parent) - pull in variables from parent window
@@ -29,8 +39,8 @@ module RespondsToParent
         # window.eval - legal eval for Opera
         render :text => "<html><body><script type='text/javascript' charset='utf-8'>
           var loc = document.location;
-          with(window.parent) { setTimeout(function() { window.eval('#{self.class.helpers.escape_javascript script}'); window.loc && loc.replace('about:blank'); }, 1) }
-        </script></body></html>"
+          with(window.parent) { setTimeout(function() { window.eval('#{script}'); if (typeof(loc) !== 'undefined') loc.replace('about:blank'); }, 1) };
+          </script></body></html>".html_safe
       end
     end
     alias respond_to_parent responds_to_parent
